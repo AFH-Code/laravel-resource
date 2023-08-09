f546a1a2c2865fedc5b82c7a0d99b176d2e05c5c
NZTY2298 <noel.kenfack@yahoo.fr>
2023-08-09 16:48:14 +0100

    update

---
+++ resources/views/confirm.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+ 
+@section('contenu')
+    <div class="container mt-2">
+        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+            <strong>Super !</strong> Données enregistrées avec succès.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    </div>
+@endsection
